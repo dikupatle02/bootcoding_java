e01e7e980057ec6dd8b05e718a2955e7409a7e61
dikupatle02 <dikupatle23@gmail.com>
2022-08-19 16:50:06 +0530

    Created class

---
+++ bootcoding_java/src/com/bootcoding/basic/EvenOdd.java
@@ -0,0 +1,14 @@
+package com.bootcoding.basic;
+
+public class EvenOdd
+{
+    public static void main(String[]args)
+    {
+      System.out.print("Enter a number:");
+      int num=10;
+      if(num% 2 ==0)
+          System.out.println(num +"is even");
+      else
+          System.out.println(num + "is odd");
+    }
+}
