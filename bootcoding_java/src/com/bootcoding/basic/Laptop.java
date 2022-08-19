9ebc142d896ab10ec0fd24f8ebd478847ea196bf
dikupatle02 <dikupatle23@gmail.com>
2022-08-19 20:14:09 +0530

    Laptop

---
+++ bootcoding_java/src/com/bootcoding/basic/Laptop.java
@@ -0,0 +1,16 @@
+package com.bootcoding.basic;
+
+public class Laptop {
+    String color;
+    int size;
+    float price;
+    String unit;
+
+    String getColor() {
+        return color;
+    }
+
+    void start() {
+
+    }
+}
