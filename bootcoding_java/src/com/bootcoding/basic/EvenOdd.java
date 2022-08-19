d4ed6eca80a8471e0b3cd0a8b0a74393c4b32b54
dikupatle02 <dikupatle23@gmail.com>
2022-08-19 16:53:55 +0530

    EvenOdd

--- bootcoding_java/src/com/bootcoding/basic/EvenOdd.java
+++ bootcoding_java/src/com/bootcoding/basic/EvenOdd.java
@@ -6,7 +6,7 @@ public class EvenOdd
     {
       System.out.print("Enter a number:");
       int num = 10;
-      if(num% 2 ==0)
+      if(num% 2 == 0)
           System.out.println(num + "is even");
       else
           System.out.println(num + "is odd");
