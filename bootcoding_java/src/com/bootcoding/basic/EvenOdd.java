feb5fd668aec3932e36497c2c2ff9013fed6a0f6
dikupatle02 <dikupatle23@gmail.com>
2022-08-19 16:55:32 +0530

    EvenOdd

--- bootcoding_java/src/com/bootcoding/basic/EvenOdd.java
+++ bootcoding_java/src/com/bootcoding/basic/EvenOdd.java
@@ -5,10 +5,10 @@ public class EvenOdd
     public static void main(String[]args)
     {
       System.out.print("Enter a number:");
-      int num = 10;
+      int num = 10 ;
       if(num% 2 == 0)
-          System.out.println(num + "is even");
+          System.out.println(num + " is even");
       else
-          System.out.println(num + "is odd");
+          System.out.println(num + " is odd");
     }
 }
