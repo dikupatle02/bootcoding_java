a5c39de71b601e201ba821df8339867ce6f460c8
dikupatle02 <dikupatle23@gmail.com>
2022-08-21 10:27:37 +0530

    even odd test

---
+++ bootcoding_java/test/com/bootcoding/basic/EvenOddTest.java
@@ -0,0 +1,11 @@
+package com.bootcoding.basic;
+
+import org.junit.Test;
+
+public class EvenOddTest {
+    @Test
+   public void testCase1(){
+     EvenOdd s1 = new EvenOdd();
+     s1.start();
+    }
+}
