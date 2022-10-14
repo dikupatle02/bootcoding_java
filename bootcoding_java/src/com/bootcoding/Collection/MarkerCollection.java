3b8e0a18773ec3a5bda0505bf559957b1442c28d
dikupatle02 <dikupatle23@gmail.com>
2022-10-14 20:55:44 +0530

    All Program

--- bootcoding_java/src/com/bootcoding/Collection/MarkerCollection.java
+++ bootcoding_java/src/com/bootcoding/Collection/MarkerCollection.java
@@ -3,6 +3,7 @@ package com.bootcoding.Collection;
 import com.bootcoding.oops.Marker2;
 
 import java.util.HashMap;
+import java.util.Random;
 
 public class MarkerCollection
 {
@@ -26,13 +27,17 @@ public class MarkerCollection
 
     public static void main(String[] args)
     {
+        String[] colors ={"Black","Blue","Red","Green","Orange"};
+
+        Random random = new Random();
         MarkerCollection markerCollection= new MarkerCollection();
-        for (int i=1; i<=10; i++)
+        for (int i=1; i<=100; i++)
         {
+            int randomIndex = random.nextInt(colors.length);
             Marker2 m= new Marker2();
             m.setId(i);
             m.setCompany("Camlin " + i);
-            m.setColor("Blue");
+            m.setColor(colors[randomIndex]);
             m.setType("WhiteB" + i);
             m.setPrice((i+100)*2.5);
             markerCollection.addMarker(m);
